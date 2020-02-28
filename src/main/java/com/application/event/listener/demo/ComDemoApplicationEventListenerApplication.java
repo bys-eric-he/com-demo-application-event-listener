53f03d30777bf375f20b511fec2e41f2404c22d8
eric he <ehe@accela.com>
2020-02-28 21:23:30 +0800

    服务启动成功后，打印API请求路径。

--- src/main/java/com/application/event/listener/demo/ComDemoApplicationEventListenerApplication.java
+++ src/main/java/com/application/event/listener/demo/ComDemoApplicationEventListenerApplication.java
@@ -8,6 +8,7 @@ public class ComDemoApplicationEventListenerApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ComDemoApplicationEventListenerApplication.class, args);
+        System.out.println("http://127.0.0.1:8080/register?name=admin&password=123456");
     }
 
 }
